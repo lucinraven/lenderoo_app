d3e724da09501d543f61ec356cf31f2c14c65b91
RavL <75201120+RavL@users.noreply.github.com>
2021-05-20 16:20:52 +0400

    review-checkout page

---
+++ public/pages/review-item.php
@@ -0,0 +1,17 @@
+<?php
+/** 
+ * User: Raven Lucin <lucinraven@gmail.com>  
+ *  
+ * Date: 5/19/2021
+ * Time: 4:32 pm
+ */
+include 'includes/header.php';
+
+?>
+
+
+<?php
+
+include 'includes/header.php';
+
+?>
