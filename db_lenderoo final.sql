a034c8577cf0919ef6773af4e1107f6c0f53cede
lucinraven <79331208+lucinraven@users.noreply.github.com>
2021-06-07 23:35:17 +0400

    Add files via upload

--- db_lenderoo final.sql
+++ db_lenderoo final.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 07, 2021 at 09:00 PM
+-- Generation Time: Jun 07, 2021 at 09:33 PM
 -- Server version: 10.4.14-MariaDB
 -- PHP Version: 7.4.10
 
@@ -66,7 +66,7 @@ INSERT INTO `cart` (`cart_id`, `user_id`, `is_active`) VALUES
 CREATE TABLE `cart_products` (
   `id` int(11) NOT NULL,
   `product_id` int(11) DEFAULT NULL,
-  `quantity` int(11) DEFAULT NULL,
+  `duration` int(11) DEFAULT NULL,
   `cart_id` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -74,7 +74,7 @@ CREATE TABLE `cart_products` (
 -- Dumping data for table `cart_products`
 --
 
-INSERT INTO `cart_products` (`id`, `product_id`, `quantity`, `cart_id`) VALUES
+INSERT INTO `cart_products` (`id`, `product_id`, `duration`, `cart_id`) VALUES
 (1, 1, 1, 1),
 (2, 2, 3, 2),
 (3, 1, 1, 2),
@@ -144,8 +144,7 @@ CREATE TABLE `fav` (
 --
 
 INSERT INTO `fav` (`fav_id`, `product_id`, `user_id`, `created_at`) VALUES
-(2, 2, 3, '2021-06-07 20:48:58'),
-(3, 3, 3, '2021-06-07 21:06:49');
+(4, 3, 3, '2021-06-07 23:22:04');
 
 -- --------------------------------------------------------
 
@@ -232,9 +231,12 @@ CREATE TABLE `product` (
 INSERT INTO `product` (`product_id`, `product_title`, `product_name`, `description`, `age`, `technicalDesc`, `price`, `category_id`, `condition_id`, `status`, `click_counter`, `lender_id`, `max_lend_duration`) VALUES
 (1, 'Selling 6 person tent', 'Coleman cabin tent', 'The best tent', '0-1 month', 'Weight: 13kg,Capacity: 6 person', 10.5, 1, 3, 'Available', 41, 1, 15),
 (2, 'Renting outdoor grill for a reasonable price', 'Imperial home non-stick grill pan', 'Nonstick pan that uses a state of the art materials. highly durable and can be easily cleaned.', 'Brand New', 'Weight: 5kg,', 15, 9, 3, 'Available', 54, 2, 30),
-(3, 'Barbeque renting set for grabs!', 'Barbeque set', 'Enter description', '1-6 months', 'Weight: 3.5,Capacity: Tons', 30, 1, 3, 'Available', 39, 3, 7),
-(4, 'Fishing set for rent! for a reasonable price.', 'Fishing rod set', 'Fishing rod for rent, contact me for more information', '6-12 months', 'Weight: 3kg', 20, 4, 2, 'Available', 1, NULL, 2),
-(7, '1 person kayaking boat', 'Kayaking boat', 'Kayaking boat for outdoor enthusiast and people passionate for exploration', '1-2 years', 'Weight: 3kg,Length: 6m,Color: Yellow', 50, 8, 5, 'Available', 0, NULL, 5);
+(3, 'Barbeque renting set for grabs!', 'Barbeque set', 'Enter description', '1-6 months', 'Weight: 3.5,Capacity: Tons', 30, 1, 3, 'Available', 42, 3, 7),
+(4, 'Fishing set for rent! for a reasonable price.', 'Fishing rod set', 'Fishing rod for rent, contact me for more information', '6-12 months', 'Weight: 3kg', 20, 4, 2, 'Available', 1, 4, 2),
+(7, '1 person kayaking boat', 'Kayaking boat', 'Kayaking boat for outdoor enthusiast and people passionate for exploration', '1-2 years', 'Weight: 3kg,Length: 6m,Color: Yellow', 50, 8, 5, 'Available', 0, 2, 5),
+(8, 'Camping bag for outdoor explorers', 'Camping bag', 'Camping bag for outdoor explorers.', 'Brand New', 'Weight: 3kg,', 15, 1, 3, 'Available', 0, 3, 5),
+(9, 'Camping shoes for bargain for a right price!', 'Hiking shoes', 'Hiking shoes for men and women.', 'Brand New', 'Shoe size: 40 men,Metal Toe,Weight: 1.5kg', 5, 6, 2, 'Available', 0, 4, 3),
+(10, 'Tool box set for rent', 'Tool box series mk1000', 'Tool box set for fixing mechanical issues and home furnitures.', 'Brand New', 'Weight: 5kg,', 10, 5, 2, 'Available', 0, 3, 15);
 
 -- --------------------------------------------------------
 
@@ -282,7 +284,10 @@ INSERT INTO `product_image` (`image_id`, `source`, `product_id`) VALUES
 (5, '71ydrdh37al-sl1500--500x500.jpg', 2),
 (6, 'stainless-steel-barbeque-set-500x500.jpg', 3),
 (7, 'fishing.jpg', 4),
-(9, 'hobie-proangler12-boa.jpg', 7);
+(9, 'hobie-proangler12-boa.jpg', 7),
+(10, 'osprey-aether-ag-60-hiking-backpack.jpg', 8),
+(11, 'Shoe+-+1.png', 9),
+(12, 'Tool-Cabinet-with-Wheels-Tool-Trolley-Tool-Kit-Tool-Box.jpg', 10);
 
 -- --------------------------------------------------------
 
@@ -538,7 +543,7 @@ ALTER TABLE `credit`
 -- AUTO_INCREMENT for table `fav`
 --
 ALTER TABLE `fav`
-  MODIFY `fav_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `fav_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `message`
@@ -562,7 +567,7 @@ ALTER TABLE `password_token`
 -- AUTO_INCREMENT for table `product`
 --
 ALTER TABLE `product`
-  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `product_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `product_condition`
@@ -574,7 +579,7 @@ ALTER TABLE `product_condition`
 -- AUTO_INCREMENT for table `product_image`
 --
 ALTER TABLE `product_image`
-  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT for table `product_reviews`
